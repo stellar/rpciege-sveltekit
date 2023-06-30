32b3403faf339bd68f6e6ed9d2b7a28f480cba9c
Tyler van der Hoeven <hi@tyvdh.com>
2023-06-30 14:04:13 -0400

    /shrug

--- src/routes/claim/+page.svelte
+++ src/routes/claim/+page.svelte
@@ -221,11 +221,10 @@
     }
   }
   function processAssetCode(code: string) {
-    if (code.substr(-1) === 'c') {
-      code = code
+    if (code.substr(-1) === 'c')
+      return code
       .replace(/[a-z]/g, '')
       .replace('RPCIEGE', 'RPCIEGE0')
-    }
 
     return code
   }
