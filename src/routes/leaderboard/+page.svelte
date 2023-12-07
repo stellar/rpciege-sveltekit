871715631d2b933fe06e6d7412ed290653635c7b
Tyler van der Hoeven <hi@tyvdh.com>
2023-12-07 09:11:47 -0500

    Skirmish 13 (#25)
    * add skirmish 12
    
    * add skirmish 13

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -2,7 +2,7 @@
 	import { dev } from '$app/environment';
     import { page } from '$app/stores'
 
-    const skirmishes = new Array(12)
+    const skirmishes = new Array(13)
     const fields = [
         'timestamp',
         'instructions',
